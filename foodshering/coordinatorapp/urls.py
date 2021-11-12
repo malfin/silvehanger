29f35b40eb888aedebfb49ad22632ba5f84e8847
Алексей <aiex.0115970@gmail.com>
2021-11-12 21:03:27 +0500

    Empty Project

---
+++ foodshering/coordinatorapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+import coordinatorapp.views as coordinatorapp
+
+app_name = 'coordinatorapp'
+
+urlpatterns = [
+
+]
---
+++ foodshering/donorapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+import donorapp.views as donorapp
+
+app_name = 'donorapp'
+
+urlpatterns = [
+
+]
