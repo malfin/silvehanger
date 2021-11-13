11560aa5fc7912a3664b755c023d4cdf23838af2
plagueevgeny <plagueevgeny@yandex.ru>
2021-11-13 13:50:02 +0500

    add Organ

--- foodshering/mainapp/admin.py
+++ foodshering/mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from mainapp.models import Group, Product
+from mainapp.models import Group, Product, Organ
 
 admin.site.register(Group)
+admin.site.register(Organ)
 admin.site.register(Product)
