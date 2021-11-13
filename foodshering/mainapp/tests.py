fc0d94603b1774bc5772a65aa9b5aedb8c1b8fe2
Алексей <aiex.0115970@gmail.com>
2021-11-13 10:30:31 +0500

    Jobs on doc(python_library) and using testing

--- foodshering/mainapp/tests.py
+++ foodshering/mainapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from docxtpl import DocxTemplate
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, 'ru_RU')
+date = datetime.now()
+
+doc = DocxTemplate("doc/act.docx")
+context = {
+    'name_or_fio': 'Наименование/ФИО',
+    'day': date.day,
+    'month': date.strftime("%B"),
+    'year': date.year,
+    'position': 'Должность',
+    'fio': 'ФИО',
+}
+doc.render(context)
+doc.save("doc/generated_doc.docx")
