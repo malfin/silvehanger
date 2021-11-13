1c2ebd5e3a52d9f32e25dd369a99cc91674d4e49
DexNick4501 <idesignerdisk@yandex.ru>
2021-11-13 16:53:18 +0500

    register form

--- foodshering/static/js/script.js
+++ foodshering/static/js/script.js
@@ -5,12 +5,22 @@ window.onload = function (ev) {
         let secondStep = document.querySelector('.register_form_second_step');
 
         let nextButton = document.querySelector('.button_next_step');
+        let backButton = document.querySelector('.button_back_step');
         nextButton.addEventListener('click', function (ev) {
             if (firstStep.classList.contains('dis_block') && secondStep.classList.contains('dis_none')) {
                 firstStep.classList.remove('dis_block');
                 firstStep.classList.add('dis_none');
                 secondStep.classList.remove('dis_none');
                 secondStep.classList.add('dis_block');
+            }
+        });
+
+        backButton.addEventListener('click', function (ev) {
+            if (firstStep.classList.contains('dis_none') && secondStep.classList.contains('dis_block')) {
+                firstStep.classList.remove('dis_none');
+                firstStep.classList.add('dis_block');
+                secondStep.classList.remove('dis_block');
+                secondStep.classList.add('dis_none');
             }
         });
     }
