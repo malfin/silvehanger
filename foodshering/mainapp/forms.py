d40f303ed149e98e36bff916d68a0857e544f8bc
Алексей <aiex.0115970@gmail.com>
2021-11-13 22:13:07 +0500

    Create Random Users

--- foodshering/mainapp/forms.py
+++ foodshering/mainapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from mainapp.models import LoadFiles
+from mainapp.models import LoadFiles, Group
 
 
 class LoadFileForm(forms.ModelForm):
@@ -8,6 +8,21 @@ class LoadFileForm(forms.ModelForm):
         model = LoadFiles
         fields = (
             'file',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, item in self.fields.items():
+            item.widget.attrs['class'] = 'form-control'
+            item.help_text = ''
+
+
+class CreateGroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = (
+            'name',
+            'users_volonter',
         )
 
     def __init__(self, *args, **kwargs):
